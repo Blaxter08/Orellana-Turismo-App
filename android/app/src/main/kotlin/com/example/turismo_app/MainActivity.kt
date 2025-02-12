4add57cfe7827a42117112563bbf4c4730c5d92d
Blaxter08 <107215578+Blaxter08@users.noreply.github.com>
2025-02-12 08:27:43 -0500

    Corecciones hechas, todo funciona.

--- android/app/src/main/kotlin/com/example/turismo_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/turismo_app/MainActivity.kt
@@ -2,5 +2,4 @@ package com.orellana.turismo_app_orellana
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/fahad/turismo_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.fahad.turismo_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
